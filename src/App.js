7471edf79108cc3805dbbeceae58e8d8caeb5465
Juanstor <alvarezjuansbs@gmail.com>
2024-02-01 20:58:18 -0500

    ready to add css files, fixed the array.map, change react.fragment

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import { Card } from './Card/Card.js';
 import { TuduTask } from './TuduTask/TuduTask.js';
 import { Prueba } from './Prueba/Prueba.js';
 
+const defaultTudu = [
+  { text: 'Primer proyecto de portafolio', completed: false },
+  { text: 'Completar checkeo medico', completed: true },
+  { text: 'Planear ejercicio', completed: true },
+  { text: 'Cepillar dientes', completed: true },
+];
+
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Header />
       <BackgroundImage />
 
       <Card completed={8} total={16}>
-        <TuduTask />
-        <TuduTask />
-        <TuduTask />
+        {defaultTudu.map(tudu => (
+          <TuduTask 
+            Key={tudu.text} 
+            text={tudu.text}
+            completed={tudu.completed}
+          />
+        ))}
       </Card>
 
       <Card completed={2} total={3}>
@@ -24,7 +35,7 @@ function App() {
       </Card>
       
       <Prueba />
-    </React.Fragment>
+    </>
   );
 }
 
