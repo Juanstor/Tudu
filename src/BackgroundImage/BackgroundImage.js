223a2ca512e1118816283b2c267d0b1ca32acbff
Juanstor <alvarezjuansbs@gmail.com>
2024-02-02 16:10:40 -0500

    Styles done, ready to put functionalities with react

--- src/BackgroundImage/BackgroundImage.js
+++ src/BackgroundImage/BackgroundImage.js
@@ -2,10 +2,12 @@ import './BackgroundImage.css';
 
 function BackgroundImage() {
   return (
-        <h1>
-          Este es el BackgroundImage.
-        </h1>
-
+    <section className='background'>
+      <img className='background--image' src="https://img.freepik.com/free-vector/follow-leader-concept-illustration_114360-10886.jpg?w=1380&t=st=1706907402~exp=1706908002~hmac=e4a64c610944972edbb5510fabafad10a0406bf3d09cb0a4dd046a75aa94ad10" alt='Make it real'></img>
+      <p>
+        Today's effort is <br/> tomorrow's accomplishment.
+      </p>
+    </section>
   );
 }
 
