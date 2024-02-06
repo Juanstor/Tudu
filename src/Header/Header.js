ef8fb4c731d5d481a4a6586be62536d62f085fa6
Juanstor <alvarezjuansbs@gmail.com>
2024-02-06 15:40:45 -0500

    checking

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './Header.css';
 
 function Header() {
   return (
     <div className='header'>
-      <div className='icon-container'>
+      <div onClick={(event) => console.log(event)}
+      className='icon-container'>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M3 12H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           <path d="M3 6H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
