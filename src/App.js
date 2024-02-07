66d896dbdb15267c97190af5699d9229f59d2ba3
Juanstor <alvarezjuansbs@gmail.com>
2024-02-07 18:12:15 -0500

    counting and inpud ready, fixing some problems with delete task

--- src/App.js
+++ src/App.js
@@ -5,18 +5,47 @@ import { Card } from './Card/Card.js';
 import { TuduItem } from './TuduItem/TuduItem.js';
 import { Prueba } from './Prueba/Prueba.js';
 
-const defaultTudu = [
+const defaultTudus = [
   { text: 'Primer proyecto de portafolio', completed: false },
   { text: 'Completar checkeo medico', completed: true },
   { text: 'Planear ejercicio, nada de pereza, just try it!', completed: true },
   { text: 'Cepillar dientes', completed: false },
+  { text: 'Adicional Tudu para contar cosas', completed: true },
 ];
 
 
 function App() {
 
-  // Contador de Tudus
-  // const[tudu, setCountTudus] = React.useState('defaultTudu');
+  // Contador de tudus
+  const[tudus, setTudus] = React.useState(defaultTudus);
+  const completedTudus = tudus.filter(
+    tudu => !!tudu.completed
+  ).length;
+  const totalTudus = tudus.length;
+
+  //toggle complete, Completing a Tudu with the icon check
+  const completingTudu = (text) => {
+    const newTudus = [...tudus];
+    const tuduIndex = newTudus.findIndex(
+      (tudu) => tudu.text === text
+    );
+    newTudus[tuduIndex].completed = true;
+    setTudus(newTudus);
+  };
+
+    //Deleting a tudu with the icon X
+    const deletingTudu = (text) => {
+      const newTudus = [...tudus];
+      const tuduIndex = newTudus.findIndex(
+        (tudu) => tudu.text === text
+      );
+      newTudus.splice(tuduIndex, 1);
+      setTudus(newTudus);
+    };
+
+    // Ahora es necesario que los dos estados esten en el archivo app para que se pueda comunicar y asi actualice la lista de tareas. Sin embargo hay problemas con el input, ya que se van a cambair todos los textos si se modifica un solo input. Revisar con cuidado.
+
+    //Otra es tambien cambiar el key de texto por otra manera, ya que el input cambia el texto, y el key se pierde.
 
   return (
     <>
@@ -24,12 +53,17 @@ function App() {
       <Header />
       <BackgroundImage />
 
-      <Card title={"Primer TuduCard"} completed={8} total={16}>
-        {defaultTudu.map(tudu => (
+      <Card title={"Primer TuduCard"} 
+        completed={completedTudus} 
+        total={totalTudus}
+      >
+        {defaultTudus.map(tudu => (
           <TuduItem 
             Key={tudu.text} 
             text={tudu.text}
-            // completed={completedTudus}
+            completed={tudu.completed}
+            onComplete={() => completingTudu(tudu.text)}
+            onDelete={() => deletingTudu(tudu.text)}
             
             // itemText={itemText}
             // setItemText={setItemText}
--- src/TuduItem/TuduItem.js
+++ src/TuduItem/TuduItem.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './TuduItem.css';
 
-function TuduItem({completed, text,}) {
+function TuduItem({completed, text, onComplete, onDelete}) {
 
   let [itemText, setItemText] = React.useState('');
 
   return (
     <li className="TuduItem">
 
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="24 25" fill="none">
+      <svg 
+        onClick={onComplete}
+        xmlns="http://www.w3.org/2000/svg" 
+        width="25" 
+        height="25" 
+        viewBox="0 0 25 25" 
+        fill="none">
         <path d="M11.9993 22.5342C17.5221 22.5342 21.9993 18.0571 21.9993 12.5342C21.9993 7.01139 17.5221 2.53424 11.9993 2.53424C6.47642 2.53424 1.99927 7.01139 1.99927 12.5342C1.99927 18.0571 6.47642 22.5342 11.9993 22.5342Z" stroke="black" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
 
@@ -21,14 +27,20 @@ function TuduItem({completed, text,}) {
           console.log("escribiste en el imput")
           setItemText(event.target.value);
         }}
-
-      />
+        
+        />
 
       <p>
         {text}
       </p>
 
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <svg 
+        onClick={onDelete}
+        xmlns="http://www.w3.org/2000/svg" 
+        width="25" 
+        height="25" 
+        viewBox="0 0 25 25" 
+        fill="none">
         <path d="M15.9993 9.53424L7.99927 17.5342" stroke="#909090" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M7.99927 9.53424L15.9993 17.5342" stroke="#909090" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>  
