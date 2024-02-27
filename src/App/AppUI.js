b2ba67c4fc246607b52a596504de99e41c065857
Juanstor <alvarezjuansbs@gmail.com>
2024-02-27 11:52:48 -0500

    apply useContext to organize AppUI.js

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { TuduContext } from '../Context/Context.js';
 import { Header } from '../Header/Header.js';
 import { BackgroundImage } from '../BackgroundImage/BackgroundImage.js';
 import { Card } from '../Card/Card.js';
@@ -6,48 +8,45 @@ import { Prueba } from '../Prueba/Prueba.js';
 import { EmptyTudu } from '../EmptyTudu/EmptyTudu.js';
 import { LoadingTudu } from '../LoadingTudu/LoadingTudu.js';
 import { ErrorTudu } from '../ErrorTudu/ErrorTudu.js';
-import { TuduContext } from '../Context/Context.js';
 
 function AppUI () {
+
+  const {
+    loading,
+    error,
+    tudus,
+    completedTudus,
+    totalTudus,
+    progressPercentage,
+    completingTudu,
+    deletingTudu,
+  } = React.useContext(TuduContext);
+
   return (
     <>
       <Header />
       <BackgroundImage />
   
-      <TuduContext.Consumer>
-        {({
-          loading,
-          error,
-          tudus,
-          completedTudus,
-          totalTudus,
-          progressPercentage,
-          completingTudu,
-          deletingTudu,
-        }) => (
-          <Card 
-            title={"Primer TuduCard"} 
-            completed={completedTudus} 
-            total={totalTudus}
-            percentage={progressPercentage}
-          >
-          {loading && <LoadingTudu />}           
-          {error && <ErrorTudu />}           
-          {(!loading && tudus.lenght === 0) && <EmptyTudu />}  
+      <Card 
+        title={"Primer TuduCard"} 
+        completed={completedTudus} 
+        total={totalTudus}
+        percentage={progressPercentage}
+      >
+        {loading && <LoadingTudu />}           
+        {error && <ErrorTudu />}           
+        {(!loading && tudus.lenght === 0) && <EmptyTudu />}  
 
-          {tudus.map(tudu => (
-            <TuduItem 
-              Key={tudu.text}
-              text={tudu.text}
-              completed={tudu.completed}
-              onComplete={() => completingTudu(tudu.text)}
-              onDelete={() => deletingTudu(tudu.text)}
-            />
-          ))}
-        </Card>
-        )}
-
-      </TuduContext.Consumer>
+        {tudus.map(tudu => (
+          <TuduItem 
+            Key={tudu.text}
+            text={tudu.text}
+            completed={tudu.completed}
+            onComplete={() => completingTudu(tudu.text)}
+            onDelete={() => deletingTudu(tudu.text)}
+          />
+        ))}
+      </Card>
       
       <Prueba />
     </>
